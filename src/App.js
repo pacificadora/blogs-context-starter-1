aba13740c0704ae7646f629c8a28f7e333283371
Aman Singhal <singhalaman198@gmail.com>
2024-10-14 08:48:00 +0530

    added useLocation and useParam hook for useEffect

--- src/App.js
+++ src/App.js
@@ -4,17 +4,35 @@ import Header from "./components/Header";
 import Pagination from "./components/Pagination";
 import { AppContext } from "./context/AppContext";
 import './App.css'
+import { Route, Routes, useLocation, useSearchParams } from "react-router-dom";
 
 export default function App() {
   const { fetchBlogPosts } = useContext(AppContext)
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
   useEffect(() => {
-    fetchBlogPosts();
-  }, [])
+    const page = searchParams.get("page") ?? 1;
+    if (location.pathname.includes("tags")) {
+      const tag = location.pathname.split('/').at(-1).replace('-', ' ');
+      fetchBlogPosts(+page, tag)
+    }
+    else if (location.pathname.includes("categories")) {
+      const category = location.pathname.split('/').at(-1).replace('-', ' ');
+      fetchBlogPosts(+page, null, category)
+    }
+    else {
+      fetchBlogPosts(+page)
+    }
+
+  }, [location.pathname, location.search]) // will have to render it whnever the path is changing or the number of page is changing.
+
+
   return (
-    <div className="w-full h-full  flex flex-col justify-center items-center gap-y-1">
-      <Header />
-      <Blogs />
-      <Pagination />
-    </div>
-  );
+    <Routes>
+      <Route path="/">{<Home />}</Route>
+      <Route path="/blog/:blogId">{<BlogPage />}</Route>
+      <Route path="/tags/:tag">{<TagPage />}</Route>
+      <Route path="/categories/:category">{<CategoryPage />}</Route>
+    </Routes>
+  )
 }
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -10,10 +10,16 @@ export function AppContextProvider({ children }) {
     const [totalPages, setTotalPages] = useState(null);
 
 
-    async function fetchBlogPosts(page = 1) {
+    async function fetchBlogPosts(page = 1, tag = null, category) {
         setLoading(true);
+        let url = `${baseUrl}?page=${page}`
+        if (tag) {
+            url += `&tag=${tag}`
+        }
+        if (category) {
+            url += `&tag=${category}`
+        }
         try {
-            let url = `${baseUrl}?page=${page}`
             const result = await fetch(url);
             const data = await result.json()
             setPageCount(data.page);
